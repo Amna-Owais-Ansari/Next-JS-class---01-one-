40f804379fcf6a20b6e97c3844d721bd1eb65100
Amna Owais Ansari <aoansari9@gmail.com>
2024-10-04 12:04:55 +0500

    Day 1 at governor House Next JS Quarter2

---
+++ README.md
@@ -0,0 +1,3 @@
+"# Next-JS-Class-01" 
+" class1_myfirstproject" 
+"# Next-JS-Class--1" 
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+export default function homepage(){
+    return(
+        <div>
+            <center>
+                <h1 style={{ color:'blue', fontSize:'30px'}}>Hello World!</h1>
+                </center>
+            <center><h2>Welcome to Next.js</h2></center>
+            <p>"This is my !st Next js project</p>
+            <button>Click Me!</button>
+            <ol>
+                <li>Next.js </li>
+                <li>React</li>
+                <li>JavaScript</li>
+                <img src="https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?cs=srgb&dl=pexels-pixabay-56866.jpg&fm=jpg" alt="image not found" />
+            </ol>
+        </div>
+    )
+}
